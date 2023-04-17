351f1e8dd011f95b539a9b2dfed24a29e13e2640
Raf staking <nardo.rafael@gmail.com>
2023-04-17 21:57:31 +0200

    Update config.mainnet.ts

--- src/config.mainnet.ts
+++ src/config.mainnet.ts
@@ -52,7 +52,7 @@ export const network: NetworkType = {
   gatewayAddress: 'https://gateway.multiversx.com',
   explorerAddress: 'https://explorer.multiversx.com',
   delegationContract:
-    'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqx8llllsxavffq'
+    'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqq5hlllls6fy43w'
 };
 
 export const delegationContractData: DelegationContractType[] = [
